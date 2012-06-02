7c46deb7332d030a4ede832b2ebf979905fea372
enano.alonso <enano.alonso@gmail.com>
2012-06-02 17:23:22 +0000

    Mejora de objetos y Manejo de textview

--- Chess/src/Controller/ControlPrincipal.py
+++ Chess/src/Controller/ControlPrincipal.py
@@ -1,6 +1,11 @@
 
 from ControlVista import *
-
+from Model.Pawn import *
+from Model.Rook import *
+from Model.Knight import *
+from Model.Bishop import *
+from Model.Queen import *
+from Model.King import *
 class ControlPrincipal():
 
  
@@ -8,17 +13,20 @@ class ControlPrincipal():
     def __init__(self):
 
         self.controlView = ControlVista(self)
-
-        self.tablero =[['TorreBN','CaballoBB','AlfilBN','DamaBB','ReyBN','AlfilBB','CaballoBN','TorreBB'],
-                   ['PeonBB','PeonBN','PeonBB','PeonBN','PeonBB','PeonBN','PeonBB','PeonBN'],
+	
+        self.tablero =[[Torre('B',(0,0)),Caballo('B',(0,1)),Alfil('B',(0,2)),Dama('B',(0,3)),Rey('B',(0,4)),Alfil('B',(0,5)),Caballo('B',(0,6)),Torre('B',(0,7))],
+                   [Peon('B',(1,0)),Peon('B',(1,1)),Peon('B',(1,2)),Peon('B',(1,3)),Peon('B',(1,4)),Peon('B',(1,5)),Peon('B',(1,6)),Peon('B',(1,7))],
                    ['Negro','Blanco','Negro','Blanco','Negro','Blanco','Negro','Blanco'],
-                   ['Blanco','Negro','Blanco','Negro','Blanco','Negro','Blanco','Negro',],
+                   ['Blanco','Negro','Blanco','Negro','Blanco','Negro','Blanco','Negro'],
                    ['Negro','Blanco','Negro','Blanco','Negro','Blanco','Negro','Blanco'],
-                   ['Blanco','Negro','Blanco','Negro','Blanco','Negro','Blanco','Negro',],
-                   ['PeonNN','PeonNB','PeonNN','PeonNB','PeonNN','PeonNB','PeonNN','PeonNB'],
-                   ['TorreNB','CaballoNN','AlfilNB','DamaNN','ReyNB','AlfilNN','CaballoNB','TorreNN']]
+                   ['Blanco','Negro','Blanco','Negro','Blanco','Negro','Blanco','Negro'],
+                   [Peon('N',(6,0)),Peon('N',(6,1)),Peon('N',(6,2)),Peon('N',(6,3)),Peon('N',(6,4)),Peon('N',(6,5)),Peon('N',(6,6)),Peon('N',(6,7))],
+                   [Torre('N',((7,0))),Caballo('N',(7,1)),Alfil('N',(7,2)),Dama('N',(7,3)),Rey('N',(7,4)),Alfil('N',(7,5)),Caballo('N',(7,6)),Torre('N',(7,7))]]
 
-        self.controlView.dibujar(self.tablero)           
+	self.casillas=['a','b','c','d','e','f','g','h']
+	self.contador=0     
+	self.controlView.dibujar(self.tablero) 
+	self.turnoBlanco=True          
 #########################################################################################################
 
 
@@ -34,104 +42,63 @@ class ControlPrincipal():
 	fila2= mov[1][0]
 	col2= mov[1][1]	
 	
+	pieza=self.tablero[fila1][col1]
+	print "pieza ",pieza
+
+	if(pieza!='Blanco' and pieza!='Negro'):
+		print "Validar movimiento"
 
 	
-	if pieza == 'T':
-		self.validarTorre(mov)
-	if pieza == 'C':
-		self.validarCaballo(mov)
-	if pieza == 'A':
-		self.validarAlfil(mov)
-	if pieza == 'D':
-		self.validarDama(mov)
-	if pieza == 'R':
-		self.validarRey(mov)
-	if pieza == 'P':
-		self.validarPeon(mov)
+		if (pieza.color=='N' and self.turnoBlanco):
+			print "no es el turno Negro"
+			return
+		if (pieza.color=='B' and (not self.turnoBlanco)):
+			print "No es el turno blanco"
+			return	
 	
-	if (self.tablero[fila2][col2] == 'Negro' or self.tablero[fila2][col2] == 'Blanco'):
-
+		print "movimiento ", mov[1]		
+		jugada=pieza.validar_Movimiento(mov[1],self.tablero)
+		print "Jugada ",jugada
+	
+	
+		#No come pieza del mismo color
+		if (self.tablero[mov[1][0]][mov[1][1]]!= 'Blanco' and self.tablero[mov[1][0]][mov[1][1]]!= 'Negro'):
+			if (self.tablero[mov[1][0]][mov[1][1]].color== self.tablero[mov[0][0]][mov[0][1]].color):
+				jugada = False
+		
+		print jugada
+		if jugada:
+	 
+			if(self.turnoBlanco):			
+				self.contador=self.contador+1
+			pieza.casilla=mov[1]
+			pieza.set_Imagen(mov[1])
 			
-			if (self.tablero[fila1][col1][-1] == 'B'):
-				print "3"
-				if (self.tablero[fila2][col2]=='Negro' or self.tablero[fila2][col2][-1]=='N'):  
-					im= self.tablero[fila1][col1][:-1]+ 'N'
-					print "IM "+im
-					self.tablero[fila2].pop(col2)
-					self.tablero[fila2].insert(col2,im)
-				else:
-					self.tablero[fila2].pop(col2)		
-					self.tablero[fila2].insert(col2,self.tablero[fila1][col1])
-	
-				self.tablero[fila1].pop(col1)
-				self.tablero[fila1].insert(col1,'Blanco')
+			
+			self.tablero[fila2].pop(col2)
+			self.tablero[fila2].insert(col2,pieza)
 				
-			
-			else:	
-				print "4"
-				if (self.tablero[fila2][col2]=='Blanco' or self.tablero[fila2][col2][-1]=='B'):  
-					im= self.tablero[fila1][col1][:-1]+ 'B'
-					self.tablero[fila2].pop(col2)
-					self.tablero[fila2].insert(col2,im)
-				else:
-					self.tablero[fila2].pop(col2)		
-					self.tablero[fila2].insert(col2,self.tablero[fila1][col1])
-
-				self.tablero[fila1].pop(col1)			
+				
+			self.tablero[fila1].pop(col1)
+			if((fila1+col1)%2)==0:
 				self.tablero[fila1].insert(col1,'Negro')
-
-			print self.tablero
-
-			self.controlView.dibujar(self.tablero) 
+			else:	
+				self.tablero[fila1].insert(col1,'Blanco')
+			
+			
+			self.controlView.dibujar(self.tablero)
+			
+			if(self.turnoBlanco):
+				texto= str(self.contador)+'. '+pieza.name+self.casillas[col2]+str(fila2+1) 
+			else:
+				texto='  '+pieza.name+self.casillas[col2]+str(fila2+1)+'\n' 
+			self.controlView.set_text(texto)
+			texto=''
+			self.turnoBlanco= not self.turnoBlanco
         return
 #########################################################################################################
 
 
 
 
-#########################################################################################################
-    def validarTorre(self,mov):
-	print "Validar torre"
 
-#########################################################################################################
-
-
-#########################################################################################################
-    def validarCaballo(self,mov):
-	print "Validar caballo"
-
-#########################################################################################################
-
-
-#########################################################################################################
-    def validarAlfil(self,mov):
-	print "Validar alfil"
-
-#########################################################################################################
-
-
-
-#########################################################################################################
-    def validarDama(self,mov):
-	print "Validar dama"
-
-#########################################################################################################
-
-
-#########################################################################################################
-    def validarRey(self,mov):
-	print "Validar rey"
-
-#########################################################################################################
-
-
-#########################################################################################################
-    def validarPeon(self,mov):
-	print "Validar peon"
-	jugada = False
-	if(mov[0][1]+1)==mov[1][1] and (self.tablero[mov[1][0]][mov[1][1]]=='Blanco' or self.tablero[mov[1][0]][mov[1][1]]=='Negro'):
-		jugada=True
-	if (mov[0][1] == 1 and mov[1][1]==3) and (self.tablero[mov[1][0]][mov[1][1]]=='Blanco' or self.tablero[mov[1][0]][mov[1][1]]=='Negro'):
-		jugaa=True
-	
-#########################################################################################################
--- Chess/src/Model/Queen.py
+++ Chess/src/Model/Queen.py
@@ -1,6 +1,96 @@
 
-__author__="1"
-__date__ ="$27-may-2012 14:42:36$"
 
-if __name__ == "__main__":
-    print "Hello";
+class Dama():
+
+#####################################################################################################################
+	def __init__(self,color,casilla):
+
+		self.color=color
+		self.casilla=casilla
+		self.name='D'
+
+		self.set_Imagen(self.casilla)
+
+
+#####################################################################################################################
+
+
+
+
+#####################################################################################################################
+
+	def set_Imagen(self,casilla):
+		
+		if ((casilla[0]+casilla[1])%2) == 0 :
+			self.imagen= 'Images/set1/Dama'+self.color+'N'+'.jpg'
+		else: 
+			self.imagen= 'Images/set1/Dama'+self.color+'B'+'.jpg'
+#####################################################################################################################
+
+
+
+#####################################################################################################################
+	def validar_Movimiento(self, des,tablero):
+		print "Validar dama"
+		if(self.validar_torre(des,tablero) or self.validar_alfil(des,tablero)):
+			return True
+		else:
+			return False
+		
+#####################################################################################################################
+
+#####################################################################################################################
+	def validar_torre(self, des,tablero):
+		jugada = False
+		c=1
+	
+		#Mueve vertical
+		if (self.casilla[1]==des[1]):
+			jugada= True
+	   		if (self.casilla[0]>des[0]):
+				c = -1
+	   		k= self.casilla[0]+ c
+	   		while k != des[0]:	
+				if (tablero[k][self.casilla[1]] != 'Blanco' and tablero[k][self.casilla[1]]!= 'Negro'):
+					jugada= False
+				k=k+c	
+
+		#Mueve horizontal
+		if (self.casilla[0]==des[0]):
+			jugada=True
+	 		if (self.casilla[1]>des[1]):
+				c = -1
+	   		k= self.casilla[1]+ c
+	   		while k != des[1]:	
+				if (tablero[self.casilla[0]][k] != 'Blanco' and tablero[self.casilla[0]][k]!= 'Negro'):
+					jugada= False
+				k=k+c	
+
+	
+
+		return jugada
+
+#####################################################################################################################
+
+#####################################################################################################################
+	def validar_alfil(self, des,tablero):
+		jugada=False
+		d_c=1
+		d_f=1
+		fila=self.casilla[0]
+		col=self.casilla[1]
+		if(self.casilla[0]>des[0]):
+			d_f=-1	
+		if(self.casilla[1]>des[1]):
+			d_c=-1
+	
+		for k in range (abs(self.casilla[1] - des[1])):
+			fila=fila + d_f
+			col= col + d_c
+			if (des[0] == fila and des[1]==col):
+				jugada = True
+			if (tablero[fila][col] != 'Blanco' and tablero[fila][col] != 'Negro'):
+				break
+		return jugada
+
+#####################################################################################################################
